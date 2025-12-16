c420c76a1149b4bd4138310469a25fdbb3b33578
Pratik <pratik.patil@elevatitech.com>
2025-12-16 14:43:52 +0530

    updated

--- Module.py
+++
@@ -1,4 +0,0 @@
-from practice.module_test import calc   
-result = calc(5, 10)
-
-print("The sum is:", result)
---
+++ test.py
@@ -0,0 +1,3 @@
+from practice.myModule import myfunc, anotherfunc
+print(myfunc(10))        # Output: 12
+print(anotherfunc(7))    # Output: 14
